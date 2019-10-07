5cb76dcf5855c674a98000546f5df8c47c681785
pavel m <paullus325@gmail.com>
2019-10-07 17:26:20 +0300

    sync

--- tionm/tionm.go
+++ tionm/tionm.go
@@ -8,6 +8,8 @@ import (
 
 	"fmt"
 
+	"sync"
+
 	"github.com/m-pavel/go-tion/tion"
 	"github.com/muka/go-bluetooth/api"
 	"github.com/muka/go-bluetooth/bluez/profile/device"
@@ -18,11 +20,12 @@ type mTion struct {
 	cnct  chan error
 	debug bool
 
+	m sync.Mutex
 	d *device.Device1
 }
 
 func New(addr string, debug ...bool) tion.Tion {
-	nt := mTion{addr: addr}
+	nt := mTion{addr: addr, m: sync.Mutex{}}
 	nt.cnct = make(chan error)
 	if len(debug) > 0 {
 		nt.debug = debug[0]
@@ -31,6 +34,15 @@ func New(addr string, debug ...bool) tion.Tion {
 }
 
 func (n *mTion) ReadState(timeout time.Duration) (*tion.Status, error) {
+	n.m.Lock()
+	defer n.m.Unlock()
+	if c, err := n.d.GetConnected(); err != nil {
+		return nil, err
+	} else {
+		if !c {
+			return nil, errors.New("Not connected")
+		}
+	}
 	wc, err := n.d.GetCharByUUID(tion.WRITE_CHARACT)
 	if err != nil {
 		log.Println(err)
@@ -58,6 +70,15 @@ func (n *mTion) ReadState(timeout time.Duration) (*tion.Status, error) {
 }
 
 func (n *mTion) Update(s *tion.Status, timeout time.Duration) error {
+	n.m.Lock()
+	defer n.m.Unlock()
+	if c, err := n.d.GetConnected(); err != nil {
+		return err
+	} else {
+		if !c {
+			return errors.New("Not connected")
+		}
+	}
 	wc, err := n.d.GetCharByUUID(tion.WRITE_CHARACT)
 	if err != nil {
 		return err
@@ -77,6 +98,15 @@ func (n *mTion) Update(s *tion.Status, timeout time.Duration) error {
 }
 
 func (n *mTion) Connect(timeout time.Duration) error {
+	n.m.Lock()
+	defer n.m.Unlock()
+	if c, err := n.d.GetConnected(); err != nil {
+		return err
+	} else {
+		if c {
+			return nil
+		}
+	}
 	ad, err := api.GetDefaultAdapter()
 	if err != nil {
 		return err
@@ -104,6 +134,8 @@ func (n *mTion) Connect(timeout time.Duration) error {
 
 func (n *mTion) Disconnect() error {
 	if n.d != nil {
+		n.m.Lock()
+		defer n.m.Unlock()
 		defer func() {
 			n.d = nil
 		}()
